12fd916d5df0c131653b8e62de7573262bdbfe00
Achin <achin.pal7@gmail.com>
2025-11-14 20:43:39 -0500

    added product card

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Navigation from "./customer/components/Navigation/Navigation";
 import HomePage from "./pages/Homepage/HomePage";
 import Footer from "./customer/components/Footer/Footer";
+import Product from "./customer/components/Products/Product";
 
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
     <div className="">
       <Navigation></Navigation>
       <div>
-        <HomePage></HomePage>
+        {/* <HomePage></HomePage> */}
+        <Product></Product>
       </div>
       <Footer></Footer>
     </div>
